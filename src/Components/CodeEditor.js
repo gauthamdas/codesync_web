3b312bd77909fbff740141d69735e8ba6e4411cf
Gautham <gauthamd.das@gmail.com>
2023-04-18 11:42:42 +0530

    added room endpoints

--- src/Components/CodeEditor.js
+++ src/Components/CodeEditor.js
@@ -1,24 +1,136 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 import Editor from "@monaco-editor/react";
 import { useHistory } from "react-router-dom";
-
+import axios from "axios";
+import { getToken } from "../Utils/Common";
+import io from 'socket.io-client';
+const socket = io(`${process.env.REACT_APP_SOCKET}}`,{
+  reconnection: true,
+  autoConnect: false,
+  transports: ['websocket'],
+  protocol: 'http',
+});
+const languageOptions = [
+  { label: "JavaScript", value: "javascript" },
+  { label: "Python", value: "python" },
+  { label: "C++", value: "cpp" },
+];
+const codeOpts={
+  "javascript": "function newFunc(){\n  for (let i=0;i<l.length;i++){\n    console.log(m);\n  }\n  if (n==4){\n    console.log('hi');\n  }\n  else{\n    console.log('bye');\n  }\n}",
+  "python": "def newFunc():\n  for i in range(l):\n    print(m)\n  if n==4:\n    print('hi')\n  else:\n    print('bye')",
+  "cpp": "#include <iostream>\nusing namespace std;\nint main(){\n  for (int i=0;i<l.length();i++){\n    cout<<m;\n  }\n  if (n==4){\n    cout<<'hi';\n  }\n  else{\n    cout<<'bye';\n  }\n  return 0;\n}",
+}
 function CodeEditor() {
-  const [code, setCode] = useState("import Hello\ndef newFunc():\n  for i in l:\n    print(m)\n  if n==4:\n    print('hi')\n  else:\n    print('bye')\n ");
+  const [code, setCode] = useState(codeOpts);
   const monacoObjects = useRef(null);
+  const [inRoom, setInRoom] = useState(true);
+  const [availableRooms, setAvailableRooms] = useState([{roomCode: "123456",roomName: "test"}]);
   const [users, setUsers] = useState([]);
+
+  const [isReconnecting, setIsReconnecting] = useState(false);
+
   const decorationIds = useRef([]);
 
-  let history = useHistory()
+  let history = useHistory();
+  let roomCode = history.location.pathname.split("/")[2];
+
+
+  const languageRef = useRef("python");
+  const [language, setLanguage] = useState(languageRef.current);
+
+  const handleLanguageChange = (event) => {
+    languageRef.current = event.target.value;
+    setLanguage(event.target.value);
+    axios.post(`${process.env.REACT_APP_HOST}/room/changeLanguage`, {
+      token: getToken(),
+      roomCode: roomCode,
+      language: event.target.value,
+      }).then((res) => {
+        console.log(res);
+        setCode(()=>{
+          return {
+            ...code,
+            [languageRef.current]: res.data.code,
+          }
+        });
+      }).catch((err) => {
+        console.log(err);
+      });
+    // console.log(language.current,event.target.value);
+  };
+
+  const tryReconnect = useCallback(() => {
+    setTimeout(() => {
+      socket.connect()
+      
+    }, 2000);
+  }, [])
 
   useEffect(() => {
-    
-    history.location.pathname()
+    if (!socket.connected){
+      tryReconnect();
+    }
+
+    socket.io.on('close',(reason,description)=>{
+      console.log("socket closed");
+      if (reason==="transport close")tryReconnect();
+    })
+
+    socket?.on('assigned', (data) => {
+      console.log(data,localStorage.getItem('paymentId'));
+    });
+
+    // disconnect the socket when the component is unmounted
+    return () => {
+      console.log("unmounting");
+      socket.disconnect({token: getToken()});
+    };
+  }, [tryReconnect]);
+
+  useEffect(() => {
+    if (roomCode?.length !== 6) {
+      // console.log(history.location.pathname)
+      if (history.location.pathname===`/editor`) {
+        axios.post(`${process.env.REACT_APP_HOST}/room/getDetails`, {
+          token: getToken(),
+          }).then((res) => {
+            console.log(res);
+            setAvailableRooms(res.data);
+          }).catch((err) => {
+            console.log(err);
+          });
+      }
+      history.push("/editor");
+      return;
+    }
+    console.log(roomCode);
+    axios.post(`${process.env.REACT_APP_HOST}/room/join`, {
+      token: getToken(),
+      roomCode: roomCode,
+      }).then((res) => {
+        console.log(res);
+        setInRoom(true);
+      }).catch((err) => {
+        console.log(err);
+        history.push("/editor");
+      });
     return () => {
     
     }
-  }, [history.location])
+  }, [history, roomCode])
   
-
+  const createRoom = () => {
+    axios.post(`${process.env.REACT_APP_HOST}/room/create`, {
+      token: getToken(),
+      roomName: "test",
+      defaultLanguage: "python",
+      }).then((res) => {
+        console.log(res);
+        history.push(`/editor/${res.data.roomCode}`);
+      }).catch((err) => {
+        console.log(err);
+      });
+  }
   // create a array of 10 colors with opacity 0.7
   useEffect(() => {
     if (!monacoObjects.current) return;
@@ -30,31 +142,6 @@ function CodeEditor() {
 
     // create text decorations  based on user position
     const decorations = createDecorations(editor, monaco, users);
-    
-    // const selections = users.map((user, index) => {
-      //   return {
-        //     selectionStartLineNumber: user.lineNumber,
-        //     selectionStartColumn: user.column,
-        //     positionLineNumber: user.lineNumber,
-        //     positionColumn: user.column,
-        //     color: user.color
-        //   };
-        // });
-        
-        // // Create an array of decorations, one for each selection
-        // const decorations = selections.map(selection => ({
-          //   range: new monaco.Range(
-            //     selection.selectionStartLineNumber,
-            //     selection.selectionStartColumn,
-            //     selection.positionLineNumber,
-            //     selection.positionColumn,
-            //   ),
-            //   options: {
-              //     isWholeLine: false,
-              //     inlineClassName: 'custom-selection',
-              //     backgroundColor: selection.color,
-              //   }
-              // }));
               
               // Set the decorations
       const newDecorationIds = editor.deltaDecorations([], decorations);
@@ -64,7 +151,7 @@ function CodeEditor() {
       
     }, [users]);
     
-    const createDecorations = ( editor,  monaco, users) => {
+    const createDecorations = ( editor,/** @type {import("@monaco-editor/react").Monaco} */  monaco, users) => {
       const decorations = users.map((user, index) => {
         return {
           range: new monaco.Range(
@@ -76,6 +163,7 @@ function CodeEditor() {
         ),
         options: {
           inlineClassName: `color-${user.id}`,
+          stickiness: monaco.editor.TrackedRangeStickiness.GrowsOnlyWhenTypingBefore,
           hoverMessage: { value: `User: ${user.name}` 
           },
         },
@@ -107,19 +195,35 @@ function CodeEditor() {
     ]);
   };
 
-  const handleEditorChange = useCallback((value, event) => {
-    setCode(value);
-  }, []);
+  const handleEditorChange = (value, event) => {
+    console.log(languageRef.current,value)
+    // console.log(event);
+    setCode(()=>{
+      return {
+        ...code,
+        [languageRef.current]: value,
+      }
+    });
+  };
 
   return (
     <>
       <div className="editor">
-        <h2>Code Editor</h2>
+        {inRoom ? <><h2>Code Editor <small aria-haspopup={"tree"}>({'ee'})</small></h2>
+        <div className="language-dropdown">
+  <select value={language} onChange={handleLanguageChange}>
+    {languageOptions.map((option) => (
+      <option key={option.value} value={option.value}>
+        {option.label}
+      </option>
+    ))}
+  </select>
+</div>
         <Editor
           height="75vh"
           theme="vs-dark"
-          language={"python"}
-          value={code}
+          language={languageRef.current}
+          value={code[languageRef.current]}
           options={{
             fontSize: 20,
             autoFocus: true,
@@ -127,6 +231,17 @@ function CodeEditor() {
           onMount={handleEditorDidMount}
           onChange={handleEditorChange}
         />
+        </>:
+        <>
+        <div><h1 onClick={createRoom}>Create Room</h1></div>
+        <div><h1>Join Room</h1>
+        {availableRooms.map((room,index) => {
+          return <h5 key={index} onClick={() => history.push(`/editor/${room.roomCode}`)}>{room.roomName}</h5>
+        })
+        }
+        </div>
+        </>
+}
       </div>
     </>
   );
